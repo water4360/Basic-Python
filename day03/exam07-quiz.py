e039d51328a1bd8811feb8cb2f017a173f7809af
JEONG SUBIN <water4360@naver.com>
2023-08-25 18:39:49 +0900

    교집합(set)을 활용한 최대공약수

---
+++ day03/exam07-quiz.py
@@ -0,0 +1,32 @@
+# 정수 2개를 입력받아 최대 공약수 출력
+
+# 두 수의 약수를 각각 구하고,
+# 공통된 숫자를 구하고,
+# 그 중에 가장 큰 숫자 max를 구하면?
+
+num1, num2 = map(int, input('정수를 2개 입력하세요 >> ').split())
+nums = [num1, num2]
+
+divs = []
+for num in nums :
+    div = [i for i in range(1, num+1) if num % i == 0]
+    divs.append(div)
+    print(f'"{num}"의 약수 : {div}')
+
+max_common_divisor = max(set(divs[0]) & set(divs[1]))
+print('최대 공약수 : ', max_common_divisor)
+
+
+
+
+# for i in nums :
+#     print(f'{i}의 약수 : ', end='')
+#     for j in range(1, i+1) :
+#         if i % j == 0 :
+#             dividor.append(j)
+#             print(j, sep=' ', end=' ')
+#     print()
+
+
+
+
