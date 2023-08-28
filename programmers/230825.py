a4fe2640abc4e80d603e5000c10aa47b9a64d057
JEONG SUBIN <water4360@naver.com>
2023-08-28 10:20:45 +0900

    230825 강의실 변경사항 늦은 커밋

---
+++ programmers/230825.py
@@ -0,0 +1,11 @@
+#230825(금)
+
+array = [9, -1, 0]
+# def solution(array):
+#     answer = 0
+#     return answer
+sortedArray = sorted(array)
+print(sortedArray)
+idx = len(sortedArray)//2
+print(idx)
+print(sortedArray[idx])
