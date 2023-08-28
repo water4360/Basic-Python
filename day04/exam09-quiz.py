22862d4443477e23ab4e561ee0045303ed658368
JEONG SUBIN <water4360@naver.com>
2023-08-28 20:47:17 +0900

    IO, 단어 개수, 빈도수 세기 + 알파벳 순 출력완료

--- day04/exam09-quiz.py
+++ day04/exam09-quiz.py
@@ -10,23 +10,24 @@
         단어a / 15회 / 라인2, 라인3, 5, 11...
 '''
 from collections import Counter
+import re
 
 with open('input.txt', 'r') as file :
-    cnt_words = 0
     total_words = []
-
+    #라인 분할 & 소문자화 & 특수문자 제거
     for line in file :
-        words = line.rstrip('\n').split()
-        cnt_words += len(words)
-        print(words)
-        total_words += [word.lower() for word in words if word.isalpha()]
+        splited_words = line.rstrip('\n').split()
+        total_words += [re.sub(r'[^a-zA-Z]', '',word.lower()) for word in splited_words]
+        total_words.sort()
+    # print(total_words, type(total_words))
 
+    # 총 단어 개수
+    # 단어별 빈도수
     cnt_sum = 0
     for word, cnt in Counter(total_words).items() :
-        print(f'{word} : {cnt}개')
+        # if word != '' :
+        print(f'[{word}] : {cnt}회')
         cnt_sum += cnt
 
-    print('total_words : ', sorted(total_words))
-    print('cnt_sum : ', cnt_sum)
-
-    print('개수 : ', cnt_words)
+    # print('total_words : ', sorted(total_words))
+    print(f'총 단어수 : {cnt_sum}개')
